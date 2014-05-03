6ba29504611c0da4e4e29d39ab9b1b3877aabd7f
unknown <AK26092@LAS61257.spr.bz>
2014-05-02 17:49:43 -0700

    Added flash Messages

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "User should have a first name" do
+  	user = User.new
+  	assert !user.save
+  	asset !user.errors[first_name].empty?
+  end
 end
